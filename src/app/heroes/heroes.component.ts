6a1ef12dfe495c77dd8ce9b8478ef01cd54280d6
michaelsoft <michaellyk@qq.com>
2019-01-07 19:31:54 +0800

    more

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import {HEROES} from "../mock-heroes"
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -13,9 +15,23 @@ export class HeroesComponent implements OnInit {
     name: 'Michael'
   };
 
-  constructor() { }
+  heroes: Hero[];
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe( heroes => this.heroes = heroes );
   }
 
 }
+
+
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./hero"
+
+export const HEROES : Hero[] = [
+    { id: 1, name: "Michael" },
+    { id: 2, name: "Jackson" },
+    { id: 3, name: "Dangdang" },
+    { id: 4, name: "Mazha" },
+    { id: 5, name: "Super Man" },
+    { id: 6, name: "Bat Man" },
+];
+
